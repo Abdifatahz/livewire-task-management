97c3faa73d82dd4e56d84d1c9425708459065957
abdifatahz <faahid44@hotmail.com>
2021-04-14 12:04:18 +0300

    Wiredup tasks controller to views.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks=  Task::all();
+        return view("tasks.index",compact("tasks"));
+    }
 }
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,8 @@
+
+<ul class="list-group">
+    @foreach ($tasks as $task)
+    <li class="list-group-item">
+        {{$task->name}}
+    </li>
+    @endforeach
+</ul>
