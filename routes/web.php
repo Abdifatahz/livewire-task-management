9f3720d7eef0f75cc8c3b1665c395f6d7d147c39
abdifatahz <faahid44@hotmail.com>
2021-04-15 11:58:06 +0300

    Changed main route to tasks page.

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('tasks',[TaskController::class,'index']);
+Route::get('/',[TaskController::class,'index']);
 
 
 Auth::routes();
